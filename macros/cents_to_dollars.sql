5896c2df8083dc5606364d66bb89c9cffb269487
lynxiondev <lynxion.dev@gmail.com>
2026-03-27 15:49:15 -0300

    Actualizar cast_to_numeric.sql

--- macros/cents_to_dollars.sql
+++ macros/cents_to_dollars.sql
@@ -1,3 +1,3 @@
-{% macro cents_to_dollars(column_name, decimal_places=2) -%}
+{% macro cast_to_numeric(column_name, decimal_places=2) -%}
     round( cast( ({{ column_name }}) as numeric), {{ decimal_places }} )
 {%- endmacro %}   
